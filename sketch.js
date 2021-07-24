09fa9f2ba6794e2aaa68e6f22e03bd99ead3ca84
KPq66dw8L <kezhkia@protonmail.com>
2021-07-24 21:32:20 +0200

    trained with known data

--- sketch.js
+++ sketch.js
@@ -8,38 +8,105 @@ function sign(n){
     }
 }
 
-class Perceptron {
-    //Constructor
+//Known training data
+class Point {
+    
     constructor() {
-        this.weigths = new Array(2);
-        //Initialize the weigths randomly
-        for (let i = 0; i < this.weigths.length; i++) {
-            this.weigths[i] = random(-1, 1);
+        this.x = random(width);
+        this.y = random(height);
+
+        if ( this.x > this.y ) {
+            this.label = 1;
+        } else {
+            this.label = -1;
         }
     }
 
-    inputs = new Array(2);
+    show() {
+        stroke(0);
+        if (this.label == 1) {
+            fill(255);
+        } else {
+            fill(0);
+        }
+        ellipse(this.x, this.y, 12, 12);
+    }
+
+}
+
+//Neuron
+class Perceptron {
+    //Constructor
+    
+    constructor() {
+        this.weights = new Array(2);
+        //Initialize the weigths randomly
+        for (let i = 0; i < this.weights.length; i++) {
+            this.weights[i] = random(-1, 1);
+        }
+        this.inputs = new Array(2);
+    }
+
+    
     guess(inputs) {
         let sum = 0;
-        for (let i = 0; i < this.weigths.length; i++){
-            sum += inputs[i]*this.weigths[i];
+        for (let i = 0; i < this.weights.length; i++){
+            sum += inputs[i]*this.weights[i];
         }
         let output = sign(sum);
-        console.log(output);
         return output;
+    }
+
+    train(inputs, target) {
+        this.lr = 0.1;
+        this.guezz = this.guess(inputs);
+        this.error = target - this.guezz;
+
+        //Tune all the weights
+        for (let y = 0; y < this.weights.length; y++) {
+            this.weights[y] += this.error * inputs[y] * this.lr;
+            
+        }
     }
 }
 
-
-
-let p;
+let brain;
+let points = new Array(100);
 function setup() {
-    createCanvas(200, 200);
-    p = new Perceptron();
-    let inputs2 = [-1, 0.5];
-    let guess = p.guess(inputs2);
+    createCanvas(700, 700);
+    brain = new Perceptron();
+    
+    for (let i = 0; i < points.length; i++){
+        points[i] = new Point();
+    }
+
+    // let inputs2 = [-1, 0.5];
+    // brain.guess(inputs2);
+    
 }
 
 function draw() {
+    background(200);
+    stroke(0);
+    line(0, 0, width, height);
+    for (let i = 0; i < points.length; i++){
+        points[i].show();
+    }
+    
+    for (let i = 0; i < points.length; i++){
+        let inputs3 = [points[i].x, points[i].y];
+        let target = points[i].label;
 
+        brain.train(inputs3, target);
+        
+        let guess2 =  brain.guess(inputs3);
+
+        if (guess2 == target) {
+            fill(0, 255, 0);
+        } else {
+            fill(255, 0, 0);
+        }
+        noStroke();
+        ellipse(points[i].x, points[i].y, 8, 8);
+    }
 }
